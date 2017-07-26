2488e08f31ba972ae9e3d56df926216d142c85fc
amnesia02 <652033693@qq.com>
2017-07-26 23:44:58 +0800

    第二章更新，替换Response等，使用框架

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # API views
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+# from rest_framework.renderers import JSONRenderer  # 被Response替代
+# from rest_framework.parsers import JSONParser  # 使用装饰器后更改
+from rest_framework.response import Response  # renders the received data into the appropriate content type
 from rest_framework import status
+from rest_framework.decorators import api_view
 from .models import Game
 from .serializers import GameSerializer
 
-
+'''
+第二章中 被Response 替代
 class JSONResponse(HttpResponse):
     """
     subclass of django,http.HttpResponse.use string as content.create a JSONRenderer instance
@@ -18,8 +21,10 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
+'''
 
 
+@api_view(['GET', 'POST'])  # 第二章添加
 @csrf_exempt  # ensure that the view sets a Cross-Site Request Forgery(CSRF) cookie
 def game_list(request):
     """ 
@@ -29,36 +34,39 @@ def game_list(request):
     if request.method == 'GET':  # the method attribute provides a string representing the HTTP verb
         games = Game.objects.all()
         games_serializer = GameSerializer(games, many=True)
-        return JSONResponse(games_serializer.data)
+        return Response(games_serializer.data)
     elif request.method == 'POST':
-        game_data = JSONParser().parse(request)
-        game_serializer = GameSerializer(data=game_data)
+        # game_data = JSONParser().parse(request) 使用装饰器后更改
+        # game_serializer = GameSerializer(data=game_data)
+        game_serializer = GameSerializer(data=request.data)
         if game_serializer.is_valid():
             game_serializer.save()
-            return JSONResponse(game_serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(game_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
 @csrf_exempt
 def game_detail(request, pk):
     try:
         game = Game.objects.get(pk=pk)
     except Game.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         game_serializer = GameSerializer(game)
-        return JSONResponse(game_serializer.data)
+        return Response(game_serializer.data)
     elif request.method == 'PUT':
-        game_data = JSONParser().parse(request)
-        game_serializer = GameSerializer(game, data=game_data)
+        # game_data = JSONParser().parse(request)  # 使用装饰器后更改
+        # game_serializer = GameSerializer(game, data=game_data)
+        game_serializer = GameSerializer(game, data=request.data)
         if game_serializer.is_valid():
             game_serializer.save()
-            return JSONResponse(game_serializer.data)
-        return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(game_serializer.data)
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         game.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
